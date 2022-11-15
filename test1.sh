96b686652f94c2170fbb5d1701bc81fca88478f6
Ubuntu <ubuntu@ip-172-31-44-15.ap-south-1.compute.internal>
2022-11-15 07:07:44 +0000

    add test1 file to git

---
+++ test1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#script  to delete old builds and delete old files
+ls -lrt system1 | awk 'NR>1 {print $NF}' > output10
+count=`cat ./output10 | wc -l`
+while read line
+do
+	if [ $count -gt 10 ]
+	then
+		rm -rf system1/$line
+		count=`expr $count - 1`
+	fi
+done < output10 
