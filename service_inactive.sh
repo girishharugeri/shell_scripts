5bb3ebe82168a9ebe80bded97d8086a012a0a53a
Girish <harugeri.girish@gmail.com>
2022-11-26 11:15:58 +0000

    script service-inactive

--- service_inactive.sh
+++ service_inactive.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-services="tomcat jenkins"
-for i in services
-do
-	ps -C $i
-	if [ $? -ne 0 ]
-	then
-		echo "ALERT! $i has stopped, need to restart" | mail -s "service inactivity" harugeri.girish@gmail.com
-		echo "sent mail"
-	fi
-done
+tomcat=8090
+jenkins=8080
+netstat -ntlpu | grep ":$tomcat"
+if [ $? -ne 0 ]
+then
+	echo "ALERT! tomcat has stopped, need to restart" | mail -s "service tomcat inactivity" harugeri.girish@gmail.com
+	echo "sent mail"
+fi
+netstat -ntlpu | grep ":$jenkins"
+if [ $? -ne 0 ]
+then
+        echo "ALERT! jenkins has stopped, need to restart" | mail -s "service jenkins inactivity" harugeri.girish@gmail.com
+        echo "sent mail"
+fi
+
