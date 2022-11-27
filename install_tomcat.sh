a2eb1acaeb41a5bbc49db564d754484273259851
Girish <harugeri.girish@gmail.com>
2022-11-27 05:39:47 +0000

    tomcat install new

--- install_tomcat.sh
+++ install_tomcat.sh
@@ -14,6 +14,12 @@ sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.27/bin/apache-tomcat-1
 
 sudo tar -xzvf /opt/apache-tomcat-10.0.27.tar.gz
 
-#now we need to start tomcat
+#now we create link for tomcat to start/stop from anywere
 
-sudo sh /opt/apache-tomcat-10.0.27/bin/startup.sh
+sudo ln -s /opt/apache-tomcat-10.0.27/bin/startup.sh /usr/local/bin/tomcatup
+
+sudo ln -s /opt/apache-tomcat-10.0.27/bin/shutdown.sh /usr/local/bin/tomcatdown
+
+#to start tomcat 
+
+sudo tomcatup
