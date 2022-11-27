68bffc7e2ccfc8881cee93490d8813b6651e7649
girishharugeri <118253883+girishharugeri@users.noreply.github.com>
2022-11-27 19:22:07 +0530

    Update install_tomcat.sh

--- install_tomcat.sh
+++ install_tomcat.sh
@@ -4,22 +4,22 @@
 
 cd /opt
 
-sudo rm -rf apache-tomcat-10.0.27.tar.gz
+-S rm -rf apache-tomcat-10.0.27.tar.gz
 
 #first we need fetch tomcat latest version from tomcat website (at present tomcat version is 10.0.27)
 
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.27/bin/apache-tomcat-10.0.27.tar.gz
+-S wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.27/bin/apache-tomcat-10.0.27.tar.gz
 
 #now we need to unzip the apache tomcat folder 
 
-sudo tar -xzvf /opt/apache-tomcat-10.0.27.tar.gz
+-S tar -xzvf /opt/apache-tomcat-10.0.27.tar.gz
 
 #now we create link for tomcat to start/stop from anywere
 
-sudo ln -s /opt/apache-tomcat-10.0.27/bin/startup.sh /usr/local/bin/tomcatup
+-S ln -s /opt/apache-tomcat-10.0.27/bin/startup.sh /usr/local/bin/tomcatup
 
-sudo ln -s /opt/apache-tomcat-10.0.27/bin/shutdown.sh /usr/local/bin/tomcatdown
+-S ln -s /opt/apache-tomcat-10.0.27/bin/shutdown.sh /usr/local/bin/tomcatdown
 
 #to start tomcat 
 
-sudo tomcatup
+-S tomcatup
