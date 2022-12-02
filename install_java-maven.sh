c7f7a7b1324e494de7ca9eb5b5de8f3ca357ef34
girishharugeri <118253883+girishharugeri@users.noreply.github.com>
2022-12-02 13:57:20 +0530

    Update and rename install_maven.sh to install_java-maven.sh

---
+++ install_java-maven.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#now install java and maven
+
+sudo apt update
+
+sudo apt install default-jdk
+
+sudo apt install maven
--- install_maven.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#now install maven
-
-sudo apt update
-
-sudo apt install maven
