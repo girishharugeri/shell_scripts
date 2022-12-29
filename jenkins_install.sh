64c94dc4098ff5e519dce97ebb838b50e312e81e
girishharugeri <118253883+girishharugeri@users.noreply.github.com>
2022-12-29 14:19:11 +0530

    added java line

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 #Before installing jenkins we need to install java 
+#first need to update
+
+sudo apt update
+
+#now install java
+sudo apt install default-jdk
+
 #Now need to fetch latest versions from internet and add repository key to the system
 
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
